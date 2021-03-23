4f2a64ad0441936a81666b8687a8fd4dfc5e87a2
adt-hgkim <hgkim@gnmpm.com>
2021-03-23 23:04:17 +0900

    fix: test

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
-	export let name: string;
 </script>
 
 <main>
 	<h1>Color Psychology</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>hohoho</p>
 </main>
 
 <style>
