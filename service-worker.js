92ac2b7cf1dc244657781a1970cebd3099e6d14d
adt-hgkim <hakgeumkim@gmail.com>
2021-08-09 09:03:45 +0000

    Deploying to gh-pages from  @ 81a5143e40e0ce61e3c6f7921b3f58015241066b 🚀

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,37 @@
-// This is the service worker with the Cache-first network
+// Choose a cache name
+const cacheName = 'cache-v1';
+// List the files to precache
+const precacheResources = [
+  '/', 
+  '/index.html', 
+  '/favicon.png', 
+  '/global.css', 
+  '/manifest.json', 
+  '/service-worker.js', 
+  '/build/bundle.css', 
+  '/build/bundle.js', 
+  '/build/bundle.js.map'
+];
 
-const CACHE = "pwabuilder-precache";
-
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
-
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.type === "SKIP_WAITING") {
-    self.skipWaiting();
-  }
+// When the service worker is installing, open the cache and add the precache resources to it
+self.addEventListener('install', (event) => {
+  console.log('Service worker install event!');
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(precacheResources)));
 });
 
-workbox.routing.registerRoute(
-  new RegExp('/*'),
-  new workbox.strategies.CacheFirst({
-    cacheName: CACHE
-  })
-);
+self.addEventListener('activate', (event) => {
+  console.log('Service worker activate event!');
+});
+
+// When there's an incoming fetch request, try and respond with a precached resource, otherwise fall back to the network
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch intercepted for:', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) {
+        return cachedResponse;
+      }
+      return fetch(event.request);
+    }),
+  );
+});
